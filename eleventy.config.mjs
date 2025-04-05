63101c3120fa2568ece967f84445b0ccaac3874f
Damien Erambert <damien@erambert.me>
2025-04-04 21:54:03 -0700

    header, get rid of webc

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,5 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import crypto from "node:crypto";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
@@ -10,12 +11,34 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/themes");
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
-  eleventyConfig.addPlugin(pluginWebc, {
-    components: "./src/_components/**/*.webc",
-    useTransform: true,
-  });
 
   eleventyConfig.addFilter("number_locale", (number) => {
     return Number(number).toLocaleString("en-US");
   });
+
+  eleventyConfig.addPairedShortcode(
+    "os9Window",
+    (content, title, className = "") => {
+      const uid = crypto
+        .createHash("sha256")
+        .update(Date.now().toString())
+        .digest("hex")
+        .slice(0, 10);
+      return `
+    <div class="macos9-window ${className}" id=${uid}>
+      <div class="macos9-window-titlebar">
+        <button class="button close" data-action="close"></button>
+        <span class="filler"></span>
+        <span class="title-text">${title}</span>
+        <span class="filler"></span>
+        <button class="button zoom" data-action="zoom"></button>
+        <button class="button collapse" data-action="collapse"></button>
+      </div>
+      <div class="macos9-window-body">
+      ${content}        
+      </div>
+    </div>
+    `;
+    },
+  );
 }
