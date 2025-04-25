e99b26b4b30317287109936b103853fd9a843e8c
Damien Erambert <damien@erambert.me>
2025-04-25 11:34:16 -0700

    hide debug page on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 
+const isDev = import.meta.env.DEV;
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/page/1": "/",
+    ...(!isDev
+      ? {
+          "/debug": "/",
+        }
+      : {}),
   },
   devToolbar: {
     enabled: false,
