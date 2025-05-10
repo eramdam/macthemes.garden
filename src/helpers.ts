34fb3f00a76eaf8669d8c167527bc4f4d4d39b5c
Damien Erambert <damien@erambert.me>
2025-05-09 23:43:44 -0700

    static sorting

--- src/helpers.ts
+++ src/helpers.ts
@@ -53,75 +53,112 @@ type GetPageReturn = Pick<
   "url" | "lastPage" | "currentPage" | "data"
 >;
 
+export function sortThemes(
+  collection: Array<CollectionEntry<"themes">>,
+  sortOption: SortOptions = SortOptionsEnum.enum.created,
+  sortOrder: SortOrders = SortOrdersEnum.enum.asc,
+) {
+  return orderBy(
+    collection,
+    (t) => {
+      switch (sortOption) {
+        case SortOptionsEnum.enum.created:
+          return t.data.createdAt;
+        case SortOptionsEnum.enum.author:
+          if (t.data.authors.length === 0) {
+            return "z".repeat(200);
+          }
+          return t.data.authors
+            .map((t) => customSlugify(t.id))
+            .join("\n")
+            .toLowerCase();
+        case SortOptionsEnum.enum.name:
+          return customSlugify(t.data.name).toLowerCase();
+      }
+    },
+    sortOrder === SortOrdersEnum.enum.asc ? "asc" : "desc",
+  );
+}
+
 export const PAGINATION = {
   size: pageSize,
   getPage: (
     collection: Array<CollectionEntry<"themes">>,
     pageNumber: number,
-    sortOption: SortOptions = SortOptions.CREATED,
-    sortOrder: SortOrders = SortOrders.DESC,
+    sortOption: SortOptions = SortOptionsEnum.enum.created,
+    sortOrder: SortOrders = SortOrdersEnum.enum.asc,
   ): GetPageReturn => {
-    const sorted = orderBy(
-      collection,
-      (t) => {
-        switch (sortOption) {
-          case SortOptions.CREATED:
-            return t.data.createdAt;
-          case SortOptions.AUTHOR:
-            if (t.data.authors.length === 0) {
-              return "z".repeat(200);
-            }
-            return t.data.authors
-              .map((t) => customSlugify(t.id))
-              .join("\n")
-              .toLowerCase();
-          case SortOptions.NAME:
-            return customSlugify(t.data.name).toLowerCase();
-          case SortOptions.YEAR:
-            return t.data.year || 1997;
-        }
-      },
-      sortOrder === SortOrders.ASC ? "asc" : "desc",
-    );
+    const sorted = sortThemes(collection, sortOption, sortOrder);
     const pages = chunk(sorted, pageSize);
+
+    const slug = makeKeyFromSortAndOrder(sortOption, sortOrder);
 
     return {
       lastPage: pages.length,
       currentPage: pageNumber,
       data: pages[Math.max(pageNumber - 1, 0)],
       url: {
-        last: `/page/${Math.ceil(collection.length / pageSize)}`,
-        current: pageNumber === 1 ? "/" : `/page/${pageNumber}`,
+        last: `/${slug}/${Math.ceil(collection.length / pageSize)}`,
+        current: pageNumber === 1 ? "/" : `/${slug}/${pageNumber}`,
         first: pageNumber > 1 ? "/" : undefined,
-        next: pageNumber < pages.length ? `/page/${pageNumber + 1}` : undefined,
+        next:
+          pageNumber < pages.length ? `/${slug}/${pageNumber + 1}` : undefined,
         prev:
           pages.length > 1 && pageNumber > 1
-            ? `/page/${pageNumber - 1}`
+            ? `/${slug}/${pageNumber - 1}`
             : undefined,
       },
     };
   },
 };
 
-export enum SortOptions {
-  CREATED = "created",
-  NAME = "name",
-  AUTHOR = "author",
-  YEAR = "year",
-}
-export const SortOptionsEnum = z
-  .nativeEnum(SortOptions)
-  .catch(SortOptions.CREATED);
-export enum SortOrders {
-  ASC = "asc",
-  DESC = "desc",
-}
-export const SortOrdersEnum = z.nativeEnum(SortOrders).catch(SortOrders.DESC);
+const SortOptionValues = ["created", "name", "author"] as const;
+export const SortOptionsEnum = z.enum(SortOptionValues);
+export type SortOptions = z.infer<typeof SortOptionsEnum>;
 
-export function getSortAndOrderFromUrl(url: URL) {
-  const rawSort = url.searchParams.get("sort");
-  const sort = SortOptionsEnum.parse(rawSort);
-  const rawOrder = url.searchParams.get("order");
-  const order = SortOrdersEnum.parse(rawOrder);
-  return { sort, order };
+const SortOrderValues = ["asc", "desc"] as const;
+export const SortOrdersEnum = z.enum(SortOrderValues);
+export type SortOrders = z.infer<typeof SortOrdersEnum>;
+
+const sortOptionToKey = {
+  [SortOptionsEnum.enum.author]: "a",
+  [SortOptionsEnum.enum.created]: "d",
+  [SortOptionsEnum.enum.name]: "n",
+} as const;
+const keyToSortOption = {
+  d: SortOptionsEnum.enum.created,
+  a: SortOptionsEnum.enum.author,
+  n: SortOptionsEnum.enum.name,
+} as const;
+
+type SortOptionAndOrderSlug =
+  `${(typeof sortOptionToKey)[SortOptions]}-${SortOrders}`;
+
+export function slugFromSortAndOrder(slug: SortOptionAndOrderSlug): {
+  sort: SortOptions;
+  order: SortOrders;
+} {
+  const [sortKey, order] = slug.split("-");
+
+  return {
+    sort: keyToSortOption[
+      sortKey as (typeof sortOptionToKey)[SortOptions]
+    ] as SortOptions,
+    order: order as SortOrders,
+  };
 }
+
+export function makeKeyFromSortAndOrder(
+  sort: SortOptions,
+  order: SortOrders,
+): SortOptionAndOrderSlug {
+  return `${sortOptionToKey[sort]}-${order}`;
+}
+
+export const possibleSortSlugs = SortOptionsEnum.options.flatMap((option) => {
+  return SortOrdersEnum.options.flatMap((order) => {
+    const k = sortOptionToKey[option];
+    return [`${k}-${order}`];
+  });
+}) as SortOptionAndOrderSlug[];
+export const sortSlugsRegex = new RegExp(`(${possibleSortSlugs.join("|")})`);
