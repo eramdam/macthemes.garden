f61db475e58f5c9de473cfd8872a0859b28153ce
Damien Erambert <damien@erambert.me>
2025-05-09 18:17:42 -0700

    🚧 basic sorting working

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,8 @@
 import type { Page } from "astro";
-import type { CollectionEntry } from "astro:content";
+import { z, type CollectionEntry } from "astro:content";
 import archivesData from "./themes/archive-data.json" with { type: "json" };
+import { chunk, orderBy } from "lodash-es";
+import { customSlugify } from "./themesLoader";
 
 const archives = Object.keys(archivesData);
 const formatter = Intl.NumberFormat("en-US", {
@@ -46,40 +48,73 @@ export function archiveMd5(value: string) {
 }
 
 const pageSize = 51;
+type GetPageReturn = Pick<
+  Page<CollectionEntry<"themes">>,
+  "url" | "lastPage" | "currentPage" | "data"
+>;
 
-export const PAGINATION: {
-  size: number;
-  basePage: (
-    collection: Array<CollectionEntry<"themes">>,
-  ) => Pick<
-    Page<CollectionEntry<"themes">>,
-    "url" | "lastPage" | "currentPage" | "data"
-  >;
-} = {
+export const PAGINATION = {
   size: pageSize,
-  basePage: (collection) => {
+  getPage: (
+    collection: Array<CollectionEntry<"themes">>,
+    pageNumber: number,
+    sortOption: SortOptions = SortOptions.CREATED,
+    sortOrder: SortOrders = SortOrders.DESC,
+  ): GetPageReturn => {
+    console.log({ sortOrder });
+    const sorted = orderBy(
+      collection,
+      (t) => {
+        switch (sortOption) {
+          case SortOptions.CREATED:
+            return t.data.createdAt;
+          case SortOptions.AUTHOR:
+            if (t.data.authors.length === 0) {
+              return "z".repeat(200);
+            }
+            return t.data.authors
+              .map((t) => customSlugify(t.id))
+              .join("\n")
+              .toLowerCase();
+          case SortOptions.NAME:
+            return customSlugify(t.data.name).toLowerCase();
+          case SortOptions.YEAR:
+            return t.data.year || 1997;
+        }
+      },
+      sortOrder === SortOrders.ASC ? "asc" : "desc",
+    );
+    const pages = chunk(sorted, pageSize);
+
     return {
-      lastPage: Math.ceil(collection.length / pageSize),
-      currentPage: 1,
-      data: collection.slice(0, Math.ceil(collection.length / pageSize)),
+      lastPage: pages.length,
+      currentPage: pageNumber,
+      data: pages[Math.max(pageNumber - 1, 0)],
       url: {
         last: `/page/${Math.ceil(collection.length / pageSize)}`,
-        current: "/",
-        first: "/",
-        next: "/page/2",
-        prev: undefined,
+        current: pageNumber === 1 ? "/" : `/page/${pageNumber}`,
+        first: pageNumber > 1 ? "/" : undefined,
+        next: pageNumber < pages.length ? `/page/${pageNumber + 1}` : undefined,
+        prev:
+          pages.length > 1 && pageNumber > 1
+            ? `/page/${pageNumber - 1}`
+            : undefined,
       },
     };
   },
 };
 
 export enum SortOptions {
-  CREATED = "CREATED",
-  NAME = "NAME",
-  AUTHOR = "AUTHOR",
-  YEAR = "YEAR",
+  CREATED = "created",
+  NAME = "name",
+  AUTHOR = "author",
+  YEAR = "year",
 }
+export const SortOptionsEnum = z
+  .nativeEnum(SortOptions)
+  .catch(SortOptions.CREATED);
 export enum SortOrders {
   ASC = "asc",
-  DESC = "desc",
+  DESC = "asc",
 }
+export const SortOrdersEnum = z.nativeEnum(SortOrders).catch(SortOrders.DESC);
