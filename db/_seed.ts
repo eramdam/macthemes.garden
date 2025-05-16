abe82a688a7abec55e9638f9f570f62f473e35db
Damien Erambert <damien@erambert.me>
2025-05-15 22:21:57 -0700

    basic likes sorting

--- db/_seed.ts
+++ db/_seed.ts
@@ -1,27 +1,27 @@
 import { db, Like } from "astro:db";
 import { v4 } from "uuid";
 import { generateUserUUID } from "../src/helpers/dbHelpers";
+import { themesLoader } from "../src/themesLoader";
+import { random } from "lodash-es";
 
 const THEME_ID = `7ff3f73493ff`;
 const USER_IP = "1.1.1.1";
+const themes = await themesLoader();
 // https://astro.build/db/seed
 export default async function seed() {
   const userId = generateUserUUID(USER_IP);
-  await db.insert(Like).values([
-    {
-      themeId: THEME_ID,
-      id: v4(),
-      userId,
-    },
-    {
-      themeId: THEME_ID,
-      id: v4(),
-      userId,
-    },
-    {
-      themeId: THEME_ID,
-      id: v4(),
-      userId,
-    },
-  ]);
+
+  for (const theme of themes) {
+    const numberOfLikesToAdd = random(1, 2);
+
+    await db.insert(Like).values(
+      Array.from({ length: numberOfLikesToAdd }).map(() => {
+        return {
+          themeId: theme.id,
+          id: v4(),
+          userId,
+        };
+      }),
+    );
+  }
 }
