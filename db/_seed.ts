52a9a71e6be61f93eb00060da81e929bb8cb2496
Damien Erambert <damien@erambert.me>
2025-05-17 15:18:47 -0700

    🐎 more efficient seeding

--- db/_seed.ts
+++ db/_seed.ts
@@ -2,26 +2,34 @@ import { db, Like } from "astro:db";
 import { v4 } from "uuid";
 import { generateUserUUID } from "../src/helpers/dbHelpers";
 import { themesLoader } from "../src/themesLoader";
-import { random } from "lodash-es";
+import { chunk, random, sample } from "lodash-es";
 
-const THEME_ID = `7ff3f73493ff`;
 const USER_IP = "1.1.1.1";
 const themes = await themesLoader();
+const likesNumber = [1, 20, 40, 60];
 // https://astro.build/db/seed
 export default async function seed() {
   const userId = generateUserUUID(USER_IP);
+  let records = 0;
 
-  for (const theme of themes) {
-    const numberOfLikesToAdd = random(1, 2);
-
-    await db.insert(Like).values(
-      Array.from({ length: numberOfLikesToAdd }).map(() => {
-        return {
+  console.time("seed");
+  for (const themesChunk of chunk(themes, 200)) {
+    const numberOfLikesToAdd = sample(likesNumber)! + (Date.now() % 20);
+    const operations = themesChunk.flatMap((theme) => {
+      return Array.from({ length: numberOfLikesToAdd }).map(() => {
+        return db.insert(Like).values({
           themeId: theme.id,
           id: v4(),
           userId,
-        };
-      }),
-    );
+        });
+      });
+    });
+
+    records += operations.length;
+
+    // @ts-expect-error
+    await db.batch(operations);
   }
+  console.timeEnd("seed");
+  console.log(`${records} records`);
 }
